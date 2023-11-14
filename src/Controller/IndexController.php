4c4fa87cbc934e1754fc51a0c1aaaaa7788cd9ba
wefef <arthur.parent@etudidant.univ-reims.fr>
2023-11-14 22:34:52 +0100

    Suppression des paramètres d'envoie du render de la méthode index()

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,11 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+    ## Route de la page index
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+        return $this->render('index/index.html.twig');
     }
 }
