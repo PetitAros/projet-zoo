9572e61f072efb846ed7604cb00e1610014b67e6
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2023-11-22 15:58:06 +0100

    Passage d'une liste de famille d'animaux aléatoire à la page index.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\EventRepository;
+use App\Repository\FamilleAnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +12,13 @@ class IndexController extends AbstractController
 {
     ## Route de la page index
     #[Route('/', name: 'app_index')]
-    public function index(EventRepository $eventRepository): Response
+    public function index(EventRepository $eventRepository, FamilleAnimalRepository $familleAnimalRepository): Response
     {
         $events = $eventRepository->findSomeRandom(20);
+        $familles = $familleAnimalRepository->findSomeRandom(20);
         return $this->render('index/index.html.twig', [
             'events' => $events,
+            'familles' => $familles,
         ]);
     }
 }
