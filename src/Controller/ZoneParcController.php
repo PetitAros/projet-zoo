6e7fada0f590b4ff17d2c78a137ddb6fea5f9ab4
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2023-11-22 21:45:56 +0100

    Ajout de la route pour la page des détails d'une Zone

--- src/Controller/ZoneParcController.php
+++ src/Controller/ZoneParcController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\ZoneParc;
+use App\Repository\FamilleAnimalRepository;
 use App\Repository\ZoneParcRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,5 +19,12 @@ class ZoneParcController extends AbstractController
         return $this->render('zone_parc/index.html.twig', [
             'zones_Parc' => $values,
         ]);
+    }
+
+    #[Route('/zone_parc/{id}',name: 'app_zoneParc_details',requirements: ['id'=>'\d+'])]
+    public function detailsZoneParc(FamilleAnimalRepository $repoAnimal,ZoneParc $Zone,int $idZone):Response{
+        $animals=$repoAnimal->findAllByZone($idZone);
+
+        return $this->render('zone_parc/detailZoneParc.html.twig',['zone'=>$Zone,'animals'=>$animals]);
     }
 }
