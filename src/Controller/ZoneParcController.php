cb9d6db4a8f0efc0703c4ce848da5aadac1ca509
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2023-11-22 16:22:27 +0100

    Ajout de la méthode findAll et de contenu de celle-ci dans le retour de la méthode index() du contrôleur

--- src/Controller/ZoneParcController.php
+++ src/Controller/ZoneParcController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\ZoneParcRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ZoneParcController extends AbstractController
 {
-    #[Route('/zone/parc', name: 'app_zone_parc')]
-    public function index(): Response
+    #[Route('/zone_parc', name: 'app_zone_parc')]
+    public function index(ZoneParcRepository $zoneParcRepository): Response
     {
+        $values=$zoneParcRepository->findAll();
+
         return $this->render('zone_parc/index.html.twig', [
-            'controller_name' => 'ZoneParcController',
+            'zones_Parc' => $values,
         ]);
     }
 }
