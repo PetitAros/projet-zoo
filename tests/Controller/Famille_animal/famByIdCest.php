2418f4e0b70a6153a536ee169ffabc075c4e1d2f
Romain MONNEY <Romain.monney@etudiant.univ-reims.fr>
2023-11-23 21:47:28 +0100

    création fichier test familleAnimal

---
+++ tests/Controller/Famille_animal/famByIdCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Famille_animal;
+
+use App\Tests\Support\ControllerTester;
+
+class famByIdCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
---
+++ tests/Controller/Famille_animal/indexCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Famille_animal;
+
+use App\Tests\Support\ControllerTester;
+
+class indexCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
