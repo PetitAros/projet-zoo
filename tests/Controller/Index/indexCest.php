0d55cf11ba5a4b15109ec67c5b3fa6505f6d837d
Romain MONNEY <Romain.monney@etudiant.univ-reims.fr>
2023-11-23 21:47:49 +0100

    création fichier test index(page d'acceuil)

---
+++ tests/Controller/Index/indexCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Tests\Controller\Index;
+
+use App\Tests\Support\ControllerTester;
+
+class indexCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
