0887358d89d776312f94043d22164c539c43ac12
Romain MONNEY <Romain.monney@etudiant.univ-reims.fr>
2023-11-23 21:55:01 +0100

    premiers test Event

--- tests/Controller/Event/indexCest.php
+++ tests/Controller/Event/indexCest.php
@@ -12,7 +12,16 @@ class indexCest
     }
 
     // tests
-    public function tryToTest(ControllerTester $I)
+    public function correctName(ControllerTester $I)
     {
+        $I->amOnPage('/zone_parc');
+        $I->seeInTitle('Zones Parc - Zoo Parc de Laval');
     }
+
+    public function correctHttpResponse(ControllerTester $I)
+    {
+        $I->amOnPage('/zone_parc');
+        $I->seeResponseCodeIs(200);
+    }
+
 }
