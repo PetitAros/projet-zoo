e68f911b8dd74e0714bff4c6e23f67b492791d2e
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2023-11-29 16:08:26 +0100

    Correctif sur la génération d'un animal

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -3,19 +3,21 @@
 namespace App\DataFixtures;
 
 use App\Entity\Animal;
+use App\Entity\FamilleAnimal;
 use App\Factory\AnimalFactory;
 use App\Factory\FamilleAnimalFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 ;
 
-class AnimalFixtures extends Fixture
+class AnimalFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         $animals = AnimalFactory::createMany(25,function (){
             return [
-                'event' => FamilleAnimalFactory::random()];
+                'famille' => FamilleAnimalFactory::random()];
         });
         foreach ($animals as $animal) {
             if ($animal instanceof Animal) {
@@ -26,5 +28,12 @@ class AnimalFixtures extends Fixture
         // $manager->persist($product);
 
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+          FamilleAnimalFixtures::class
+        ];
     }
 }
