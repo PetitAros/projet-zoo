c0d1f895cf46ff6667bef6557e5e84a1591f6eb2
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2023-11-29 17:21:01 +0100

    Ajout de la route show au AnimalController.php

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Animal;
 use App\Repository\AnimalRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,5 +19,17 @@ class AnimalController extends AbstractController
             'controller_name' => 'AnimalController',
             'animals' => $animals,
         ]);
+    }
+
+    #[Route('/animal/{id}',
+        name: 'app_animal_show',
+        requirements:['id' => '\d+'])]
+    public function show(
+        #[MapEntity]
+        Animal $animal)
+    : Response {
+        return $this->render('animal/show.html.twig', [
+            'controller_name' => 'AnimalController',
+            'animal' => $animal]);
     }
 }
