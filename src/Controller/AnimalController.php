31767e6ff90c78eb991be49640c89eacb66c8f2d
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2023-11-29 17:12:18 +0100

    Passage de la liste de tous les animaux sur la vue d'index de cette dernière

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class AnimalController extends AbstractController
 {
     #[Route('/animal', name: 'app_animal')]
-    public function index(): Response
+    public function index(AnimalRepository $animalRepository): Response
     {
+        $animals = $animalRepository->findAll();
         return $this->render('animal/index.html.twig', [
             'controller_name' => 'AnimalController',
+            'animals' => $animals,
         ]);
     }
 }
