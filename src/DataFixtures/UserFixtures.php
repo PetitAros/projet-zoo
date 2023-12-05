b4537deabc08f705ee465192baa68f6df302dc48
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2023-12-05 18:49:29 +0100

    Génération de 2 User dans la Bd avec UserFixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        UserFactory::createOne([
+            'email' => 'louise@example.com',
+            'pnomUser' => 'Louise',
+            'nomUser' => 'Parent',
+            'password'=> 'password',
+        ]);
+        UserFactory::createOne([
+            'email' => 'Wil@example.com',
+            'pnomUser' => 'Wilfried',
+            'nomUser' => 'Noel',
+            'password'=>'test'
+        ]);
+
+        $manager->flush();
+    }
+}
