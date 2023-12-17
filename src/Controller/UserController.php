d5a5a182d14ee1386caa69717ba5bb35af148824
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2023-12-17 12:33:29 +0100

    feat: Ajout d'une route vers la page d'update de l'utilisateur

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use GuzzleHttp\Psr7\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,8 +20,6 @@ class UserController extends AbstractController
      *
      * Redirige la personne vers une page contenant les informations de l'utilisateur qui tente d'y accéder.
      * Si aucun utilisateur n'est authentitifé, redirige vers la page de conexion.
-     *
-     * @return Response
      */
     #[Route('/user', name: 'app_user')]
     public function index(): Response
@@ -29,6 +30,26 @@ class UserController extends AbstractController
 
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+
+    #[Route('/user/update', name: 'app_user_update')]
+    public function update(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $entityManager->flush();
+        }
+
+        return $this->render('user/update.html.twig', [
+            'controller_name' => 'UserController',
+            'form' => $form,
         ]);
     }
 }
