6829f58bcec14882f60e2dd376ea623f2d0cee5d
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2023-12-17 12:13:04 +0100

    docs: Documentation du UserController et de sa méthode d'index

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,14 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * UserController.
+ * Permet la redirection vers les pages relatifs à l'utilisateur (User).
+ */
 class UserController extends AbstractController
 {
+    /**
+     * Redirection vers la page de l'utilisateur.
+     *
+     * Redirige la personne vers une page contenant les informations de l'utilisateur qui tente d'y accéder.
+     * Si aucun utilisateur n'est authentitifé, redirige vers la page de conexion.
+     *
+     * @return Response
+     */
     #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
         if (!$this->isGranted('ROLE_USER')) {
             return $this->redirectToRoute('app_login');
         }
+
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
