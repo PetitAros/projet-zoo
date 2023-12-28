adcbb4f52f922a194f12a0eac5d1a80574b2736a
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2023-12-28 21:30:03 +0100

    Ajout du contrôleur pour le CRUD de l'entité ZoneParc

---
+++ src/Controller/Admin/ZoneParcCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\ZoneParc;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ZoneParcCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return ZoneParc::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
