f2ebe7ddcf4e6ab95ae14d099ed313ea340f7b9c
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2023-12-28 23:23:20 +0100

    Modification field CRUD Event

--- src/Controller/Admin/EventCrudController.php
+++ src/Controller/Admin/EventCrudController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\DateEvent;
 use App\Entity\Event;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +19,16 @@ class EventCrudController extends AbstractCrudController
         return Event::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nomEvent', 'Nom de l\'événement'),
+            IntegerField::new('nbPlaces', 'Nombre de places'),
+            TextEditorField::new('description', 'Description'),
+            ImageField::new('imgEvent', 'Image')
+                ->setBasePath('images/events')
+            ->setUploadDir('public/images/events'),
         ];
     }
-    */
 }
