3715ee7e948ff0134673f37ac06f19587af71737
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-01-02 22:37:55 +0100

    Ajout de la récupération du texte inscrit par l'utilisateur dans la barre de recherche de la liste d'event

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use App\Entity\Event;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
     #[Route('/event', name: 'app_event')]
-    public function index(EventRepository $eRep): Response
+    public function index(EventRepository $eRep, Request $request): Response
     {
+        $value = $request->query->get('search');
+        if (null == $value) {
+            $value = '';
+        }
         $events = $eRep->findAll();
 
         return $this->render('Event/index.html.twig', ['events' => $events, 'action' => 'event']);
