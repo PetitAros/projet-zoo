7704d46b77d8c1964fb6b046519dddf0753588d7
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-01-06 18:11:07 +0100

    feat: ajout de la page de création au controller

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Reservation;
+use App\Form\ReservationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,4 +19,26 @@ class ReservationController extends AbstractController
             'controller_name' => 'ReservationController',
         ]);
     }
+
+    #[Route('/reservation/create', name: 'app_reservation_create')]
+    public function create(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $user = $this->getUser();
+        $reservation = new Reservation();
+        $form = $this->createForm(ReservationType::class, $reservation);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $reservation = $form->getData();
+            $reservation->setUser($user);
+            $entityManager->persist($reservation);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('reservation/create.html.twig', ['form' => $form]);
+    }
+
+
 }
