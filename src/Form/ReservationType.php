722569506c4ddf65429b061112f86b41ceabee64
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-01-06 18:47:45 +0100

    fix: ajout des types de données dans le formulaire de Reservation

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -2,8 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Billet;
 use App\Entity\Reservation;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +17,18 @@ class ReservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('dateReservation')
-            ->add('nbPlacesAdult')
-            ->add('nbPlacesChild')
-            ->add('billet')
-            ->add('user')
+            ->add('dateReservation', DateType::class)
+            ->add('nbPlacesAdult', NumberType::class)
+            ->add('nbPlacesChild', NumberType::class)
+            ->add('billet', EntityType::class, [
+                'class' => Billet::class,
+                'required' => true,
+                'choice_label' => 'nbJours',
+                'query_builder' => function (EntityRepository $entityRepository) {
+                    return $entityRepository->createQueryBuilder('b')
+                        ->orderBy('b.nbJours', 'ASC');
+                },
+            ])
         ;
     }
 
