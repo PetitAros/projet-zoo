aaa43c2fedbf79b3e2c630d47070eea1391579c2
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-01-07 18:02:43 +0100

    test: ajout d'un test sur l'affichage d'un Event

--- tests/Controller/Event/showCest.php
+++ tests/Controller/Event/showCest.php
@@ -3,6 +3,7 @@
 
 namespace App\Tests\Controller\Event;
 
+use App\Factory\EventFactory;
 use App\Tests\Support\ControllerTester;
 
 class showCest
@@ -14,5 +15,14 @@ class showCest
     // tests
     public function tryToTest(ControllerTester $I)
     {
+    }
+
+    public function showEvent(ControllerTester $I)
+    {
+        EventFactory::createOne(['nomEvent' => 'Event très cool']);
+        $I->amOnPage('/event/1');
+        $I->seeResponseCodeIs(200);
+        $I->see('Informations complémentaires:', 'h2');
+        $I->see('Les dates prochaines:', 'h2');
     }
 }
