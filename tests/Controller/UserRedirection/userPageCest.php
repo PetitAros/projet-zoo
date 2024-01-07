3371f10526e4212b146c5e9352befcfb0e22129d
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-01-07 16:27:12 +0100

    test: ajout d'un test qui confirme l'accès à la page user si il est connecté

--- tests/Controller/UserRedirection/userPageCest.php
+++ tests/Controller/UserRedirection/userPageCest.php
@@ -3,6 +3,7 @@
 
 namespace App\Tests\Controller\UserRedirection;
 
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class userPageCest
@@ -20,5 +21,22 @@ class userPageCest
     {
         $I->amOnPage('/user');
         $I->amOnRoute('app_login');
+    }
+
+    public function userPageWorkingIfLogged(ControllerTester $I)
+    {
+        $user = UserFactory::createOne([
+            'email' => 'test@test.com',
+            'password' => 'test',
+            'phoneUser' => '0606060606',
+            'roles' => ['ROLE_USER'],
+            'nomUser' => 'Moi',
+            'pnomUser' => 'Toujours',
+        ]);
+
+        $realUser = $user->object();
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/user');
+        $I->seeInTitle('Welcome - Zoo Parc de Laval');
     }
 }
