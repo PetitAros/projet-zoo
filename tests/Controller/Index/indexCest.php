e81311faa86fd921ee0dc3a3f0a891f89f124007
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-01-07 16:34:40 +0100

    test: ajout d'un test qui vérifie que l'administrateur possède un bouton vers le CRUD

--- tests/Controller/Index/indexCest.php
+++ tests/Controller/Index/indexCest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller\Index;
 
+use App\Factory\UserFactory;
 use App\Tests\Support\ControllerTester;
 
 class indexCest
@@ -30,5 +31,22 @@ class indexCest
         $I->seeInTitle('index - Zoo Parc de Laval');
         $I->see('Nos Animation', 'h1');
         $I->see('Nos Animaux', 'h1');
+    }
+
+    public function adminsCanAccessCrud(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne([
+            'email' => 'admin@test.com',
+            'password' => 'test',
+            'phoneUser' => '0606060606',
+            'roles' => ['ROLE_ADMIN'],
+            'nomUser' => 'Admin',
+            'pnomUser' => 'Toujours',
+        ]);
+
+        $realUser = $user->object();
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/');
+        $I->see('Compte Administrateur', 'p');
     }
 }
