323a8cfa1fd72558e37ba8c5b41a234c7f5bb569
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-01-08 17:58:52 +0100

    feat: écriture du controller lié à la reservation d'events

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\AssoEventReservation;
 use App\Entity\Billet;
+use App\Entity\Event;
 use App\Entity\Reservation;
 use App\Form\ReservationType;
 use App\Repository\BilletRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,6 +31,7 @@ class ReservationController extends AbstractController
     public function choose(BilletRepository $billetRepository): Response
     {
         $billets = $billetRepository->findAll();
+
         return $this->render('reservation/choose.html.twig', ['billets' => $billets]);
     }
 
@@ -45,15 +50,45 @@ class ReservationController extends AbstractController
             $entityManager->persist($reservation);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user');
+            return $this->redirectToRoute('app_reservation_events', ['id' => $reservation->getId()]);
         }
 
-        return $this->render('reservation/create.html.twig', ['form' => $form]);
+        return $this->render('reservation/create.html.twig', ['reservation' => $reservation, 'form' => $form]);
     }
 
     #[Route('/reservation/events/{id}', name: 'app_reservation_events')]
-    public function events(#[MapEntity] Reservation $reservation, Request $request): Response
+    public function events(#[MapEntity] Reservation $reservation, EntityManagerInterface $entityManager, Request $request): Response
     {
+        $nbPlacesReserv = $reservation->getNbPlacesChild() + $reservation->getNbPlacesAdult();
 
+        $billet = $reservation->getBillet();
+        $nbJours = $billet->getNbJours();
+        $eventRepo = $entityManager->getRepository(Event::class);
+        $events = $eventRepo->findFromDateToN(\DateTime::createFromInterface($reservation->getDateReservation()), $nbJours);
+        $form = $this->createFormBuilder();
+        foreach ($events as $event) {
+            if ($event->getNbPlacesLibres() >= $nbPlacesReserv) {
+                $form->add('events', CheckboxType::class, [
+                    'value' => $event->getId(),
+                ]);
+            }
+        }
+        $form = $form->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $selectedEventsID = $form->get('events');
+            foreach ($selectedEventsID as $eventID) {
+                $reservationEvent = new AssoEventReservation();
+                $reservationEvent->setReservation($reservation);
+                $reservationEvent->setEvent($eventRepo->find($eventID));
+            }
+
+            return $this->redirectToRoute('app_user');
+        }
+
+        return $this->render('reservation/event.html.twig', [
+            'reservation' => $reservation,
+            'form' => $form,
+        ]);
     }
 }
