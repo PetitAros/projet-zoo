e39753dd7af7385912d95e976618c66579d73716
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-01-09 17:33:53 +0100

    fix: supression de l'index dans le controller

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -20,13 +20,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ReservationController extends AbstractController
 {
-    #[Route('/reservation', name: 'app_reservation')]
-    public function index(): Response
-    {
-        return $this->render('reservation/index.html.twig', [
-            'controller_name' => 'ReservationController',
-        ]);
-    }
 
     /**
      * Application du controller de Reservation.
