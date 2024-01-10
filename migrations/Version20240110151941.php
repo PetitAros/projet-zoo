7c95665282cae505c248336c1e99f88fdf7931c2
Romain Daunat <romain.daunat@etudiant.univ-reims.fr>
2024-01-10 16:22:16 +0100

    Creation d'une migration pour Habitat

---
+++ migrations/Version20240110151941.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240110151941 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE famille_animal_habitat (famille_animal_id INT NOT NULL, habitat_id INT NOT NULL, INDEX IDX_A9F51DACAEBED700 (famille_animal_id), INDEX IDX_A9F51DACAFFE2D26 (habitat_id), PRIMARY KEY(famille_animal_id, habitat_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE habitat (id INT AUTO_INCREMENT NOT NULL, lib_habitat VARCHAR(128) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE famille_animal_habitat ADD CONSTRAINT FK_A9F51DACAEBED700 FOREIGN KEY (famille_animal_id) REFERENCES famille_animal (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE famille_animal_habitat ADD CONSTRAINT FK_A9F51DACAFFE2D26 FOREIGN KEY (habitat_id) REFERENCES habitat (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE famille_animal_habitat DROP FOREIGN KEY FK_A9F51DACAEBED700');
+        $this->addSql('ALTER TABLE famille_animal_habitat DROP FOREIGN KEY FK_A9F51DACAFFE2D26');
+        $this->addSql('DROP TABLE famille_animal_habitat');
+        $this->addSql('DROP TABLE habitat');
+    }
+}
