5cf89b068eab1fd23cb5c506d3ac8fbff42b00af
Romain Daunat <romain.daunat@etudiant.univ-reims.fr>
2024-01-10 16:15:38 +0100

    Creation de HabitatFactory

---
+++ src/Factory/HabitatFactory.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Habitat;
+use App\Repository\HabitatRepository;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
+
+/**
+ * @extends ModelFactory<Habitat>
+ *
+ * @method        Habitat|Proxy create(array|callable $attributes = [])
+ * @method static Habitat|Proxy createOne(array $attributes = [])
+ * @method static Habitat|Proxy find(object|array|mixed $criteria)
+ * @method static Habitat|Proxy findOrCreate(array $attributes)
+ * @method static Habitat|Proxy first(string $sortedField = 'id')
+ * @method static Habitat|Proxy last(string $sortedField = 'id')
+ * @method static Habitat|Proxy random(array $attributes = [])
+ * @method static Habitat|Proxy randomOrCreate(array $attributes = [])
+ * @method static HabitatRepository|RepositoryProxy repository()
+ * @method static Habitat[]|Proxy[] all()
+ * @method static Habitat[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static Habitat[]|Proxy[] createSequence(iterable|callable $sequence)
+ * @method static Habitat[]|Proxy[] findBy(array $attributes)
+ * @method static Habitat[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static Habitat[]|Proxy[] randomSet(int $number, array $attributes = [])
+ */
+final class HabitatFactory extends ModelFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function getDefaults(): array
+    {
+        return [
+            'libHabitat' => self::faker()->text(128),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): self
+    {
+        return $this
+            // ->afterInstantiate(function(Habitat $habitat): void {})
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Habitat::class;
+    }
+}
