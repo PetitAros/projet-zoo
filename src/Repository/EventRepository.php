1d740776df17d14ee6747860f24acb8d81b4bef9
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-01-10 23:04:48 +0100

    fix: modification de la casse pour passer en prod

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -76,8 +76,8 @@ class EventRepository extends ServiceEntityRepository
         --$nbJours;
 
         $request = $this->createQueryBuilder('event')
-            ->innerJoin('App:assoEventDateEvent', 'aed', 'WITH', 'event = aed.event')
-            ->innerJoin('App:dateEvent', 'de', 'WITH', 'aed.dateEvent = de')
+            ->innerJoin('App:AssoEventDateEvent', 'aed', 'WITH', 'event = aed.event')
+            ->innerJoin('App:DateEvent', 'de', 'WITH', 'aed.dateEvent = de')
             ->where("de.dateEvent BETWEEN :date AND DATE_ADD(:date,:jours, 'day')")
             ->setParameter('date', $date->format('Y-m-d'))
             ->setParameter('jours', $nbJours);
